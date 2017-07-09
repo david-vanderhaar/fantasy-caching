10124aeb1cee1af3dcc5a1759f9fa1d24d3e24cc
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-07-09 16:45:31 -0400

    fix range movement

--- app/js/adventurer.js
+++ app/js/adventurer.js
@@ -80,7 +80,16 @@ function successAdventurerWatch(position) {
       };
 
       //Update encounter range to adventurer
-      adventurerEncounterRange.center = pos;
+      adventurerEncounterRange = new google.maps.Circle({
+      strokeColor: '#0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: pos,
+      radius: 10
+      });
       console.log(pos);
       console.log(watchId);
       // console.log(consoleDisplay);
