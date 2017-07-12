c5bc7b48022792875e83168f61c6f707601d39fc
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-07-12 11:18:13 -0400

    moved svg icons for map symbols to singular object in map-icons.js

--- app/js/monstersV2.js
+++ app/js/monstersV2.js
@@ -1,1 +1,11 @@
 var Monster = new Interactable;
+
+//SVG Shape of Interactable Icon
+        Monster.mapSymbol = {
+                  path: mapIcon['monster']['slime'],
+                  fillColor: 'white',
+                  fillOpacity: 0.8,
+                  scale: .15,
+                  strokeColor: 'black',
+                  strokeWeight: 1
+                };
